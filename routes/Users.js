e7e0edfe791f48882d701ee06b13af9d20e62968
1lookgod <371164382@qq.com>
2020-06-11 21:47:06 +0800

    a

--- routes/Users.js
+++ routes/Users.js
@@ -2,8 +2,11 @@
 const express = require("express");
 const users = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
+
+process.env.SECRET_KEY = "secret";
 
 users.get("/test", (req, res) => {
   res.send({ msg: "hello world" });
@@ -39,6 +42,36 @@ users.post("/register", (req, res) => {
     })
     .catch((err) => {
       res.send("error:" + err);
+    });
+});
+
+users.post("/login", (req, res) => {
+  //得到数据 去表里查
+  User.findOne({ where: { email: req.body.email } })
+    .then((user) => {
+      //查到用户
+      if (user) {
+        //密码匹配
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          // res.send("登录成功");
+          //生成token
+          let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
+            expiresIn: 1440,
+          });
+          res.send(token);
+        } else {
+          res.send("密码错误");
+        }
+      } else {
+        res.status(400).json({
+          error: "用户不存在",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(400).json({
+        error: err,
+      });
     });
 });
 
